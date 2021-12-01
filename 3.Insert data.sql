766828c3f20717609f2be4295035d83aedb3c769
rifqihz <rifqi977@gmail.com>
2021-12-01 19:33:16 +0700

    Add fuel_option and vehicle_type tables , fuel_option and vehicle_type data , create view and stored procedure query

--- 3.Insert data.sql
+++ 3.Insert data.sql
@@ -21,3 +21,21 @@ INSERT INTO CAR_RENTAL_SCHEMA.vehicle (ID_VEHICLE, brand, model, model_year, mil
 INSERT INTO CAR_RENTAL_SCHEMA.vehicle (ID_VEHICLE, brand, model, model_year, mileage, color, vehicle_type_id, current_location_id) VALUES (4, 'Hyundai', 'Elantra', 2018, 35479, 'black', 2, 1);
 INSERT INTO CAR_RENTAL_SCHEMA.vehicle (ID_VEHICLE, brand, model, model_year, mileage, color, vehicle_type_id, current_location_id) VALUES (5, 'Volkswagen', 'Jetta', 2019, 2032, 'light gray', 3, 3);
 INSERT INTO CAR_RENTAL_SCHEMA.vehicle (ID_VEHICLE, brand, model, model_year, mileage, color, vehicle_type_id, current_location_id) VALUES (6, 'Toyota', 'RAV4', 2018, 12566, 'white', 4, 7);
+
+-- Tabel Vehicle Type
+INSERT INTO CAR_RENTAL_SCHEMA.vehicle_type (ID_VEHICLE_TYPE, name, rental_value) VALUES (1, 'Economy', 26.77);
+INSERT INTO CAR_RENTAL_SCHEMA.vehicle_type (ID_VEHICLE_TYPE, name, rental_value) VALUES (2, 'Intermediate', 29.45);
+INSERT INTO CAR_RENTAL_SCHEMA.vehicle_type (ID_VEHICLE_TYPE, name, rental_value) VALUES (3, 'Standard', 34.81);
+INSERT INTO CAR_RENTAL_SCHEMA.vehicle_type (ID_VEHICLE_TYPE, name, rental_value) VALUES (4, 'Economy SUV', 37.48);
+
+-- Tabel Fuel Option
+INSERT INTO CAR_RENTAL_SCHEMA.fuel_option (ID_FUEL_OPTION, name, description) VALUES (1, 'Pre-pay', 'Customer pays in advance for a full tank of fuel, so they can return back with an empty tank of fuel, without the hassle of last minute stops and purchasing the fuel.');
+INSERT INTO CAR_RENTAL_SCHEMA.fuel_option (ID_FUEL_OPTION, name, description) VALUES (2, 'Self-Service', 'Customer will get full tank of fuel with the rental car and must return it back with the full tank of fuel.');
+INSERT INTO CAR_RENTAL_SCHEMA.fuel_option (ID_FUEL_OPTION, name, description) VALUES (3, 'Market', 'Customer gets the car with full tank of fuel but pays for fuel at market price based on fuel usage.  ');
+
+-- Tabel Rental
+INSERT INTO CAR_RENTAL_SCHEMA.rental (ID_RENTAL, start_date, end_date, customer_id, vehicle_type_id, fuel_option_id, pickup_location_id, drop_off_location_id) VALUES (1, '2018-07-14', '2018-07-23', 1, 2, 1, 3, 5);
+INSERT INTO CAR_RENTAL_SCHEMA.rental (ID_RENTAL, start_date, end_date, customer_id, vehicle_type_id, fuel_option_id, pickup_location_id, drop_off_location_id) VALUES (2, '2018-07-10', '2018-07-12', 2, 1, 2, 1, 2);
+INSERT INTO CAR_RENTAL_SCHEMA.rental (ID_RENTAL, start_date, end_date, customer_id, vehicle_type_id, fuel_option_id, pickup_location_id, drop_off_location_id) VALUES (3, '2018-06-30', '2018-07-20', 3, 1, 3, 4, 6);
+INSERT INTO CAR_RENTAL_SCHEMA.rental (ID_RENTAL, start_date, end_date, customer_id, vehicle_type_id, fuel_option_id, pickup_location_id, drop_off_location_id) VALUES (4, '2018-06-10', '2018-07-02', 4, 4, 2, 2, 7);
+INSERT INTO CAR_RENTAL_SCHEMA.rental (ID_RENTAL, start_date, end_date, customer_id, vehicle_type_id, fuel_option_id, pickup_location_id, drop_off_location_id) VALUES (5, '2018-07-14', '2018-07-27', 5, 3, 3, 5, 3);
