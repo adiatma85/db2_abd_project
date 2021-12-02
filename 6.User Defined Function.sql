313839bccfc7350493122f055cb4cad719155e4b
FarhanFDjabari <ffadhilah8@gmail.com>
2021-12-03 00:03:41 +0800

    Fix Trigger and SP Query

--- 6.User Defined Function.sql
+++ 6.User Defined Function.sql
@@ -6,27 +6,25 @@ CREATE FUNCTION RENTAL_BY_PICKUP_AND_DROPOFF(pickup_date DATE, dropoff_date DATE
         PICKUP_LOC VARCHAR(80), 
         DROPOFF_LOC VARCHAR(80)
         ) BEGIN 
-            RETURN 
-                SELECT 
-                    CONCAT_WS(',', c.first_name, c.last_name) AS 'Customer Name',
-                    vt.name AS 'Car Type',
-                    fo.name AS 'Fuel Option Selected',
-                    CONCAT_WS(',', l1.city, l1.state) AS 'Pickup Location',
-                    CONCAT_WS(',', l2.city, l2.state) AS 'Drop-off Location'
+            SELECT CONCAT(CONCAT(c.first_name, ", "), c.last_name) AS "Customer Name",
+                vt.name AS "Car Type",
+                fo.name AS "Fuel Option Selected",
+                CONCAT(CONCAT(l1.city, ', '), l1.state) AS "Pickup Location",
+                CONCAT(CONCAT(l2.city, ', '), l2.state) AS "Drop-off Location"
                 FROM
-                    rental AS r
-                        JOIN
-                    customer AS c ON r.customer_id = c.id
-                        JOIN
-                    vehicle_type AS vt ON r.vehicle_type_id = vt.id
-                        JOIN
-                    fuel_option AS fo ON r.fuel_option_id = fo.id
-                        JOIN
-                    location AS l1 ON r.pickup_location_id = l1.id
-                        JOIN
-                    location AS l2 ON r.drop_off_location_id = l2.id
+                CAR_RENTAL_SCHEMA.rental AS r
+                JOIN
+                CAR_RENTAL_SCHEMA.customer AS c ON r.customer_id = c.id
+                JOIN
+                CAR_RENTAL_SCHEMA.vehicle_type AS vt ON r.vehicle_type_id = vt.id
+                JOIN
+                CAR_RENTAL_SCHEMA.fuel_option AS fo ON r.fuel_option_id = fo.id
+                JOIN
+                CAR_RENTAL_SCHEMA.location AS l1 ON r.pickup_location_id = l1.id
+                JOIN
+                CAR_RENTAL_SCHEMA.location AS l2 ON r.drop_off_location_id = l2.id
                 WHERE
-                    r.start_date = pickup_date
-                    AND
-                    r.end_date = end_date;
-        END
+                r.start_date = pickup_date
+                AND
+                r.end_date = end_date;             
+        END@
